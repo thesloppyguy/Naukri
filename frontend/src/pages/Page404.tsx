1842310ed83cbc5c40f8489af4129a80bf6c271b
thesloppyguy <sahilmandal@outlook.com>
2024-01-10 13:52:04 +0530

    404

--- frontend/src/pages/Page404.tsx
+++ frontend/src/pages/Page404.tsx
@@ -1,4 +1,11 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+const Page404 = () => {
+  return <div>Page404</div>;
+};
+
+export default Page404;
